87626e2fe0a849e9c0768d1ac81eb99fd1666c0b
hareeqi <hareeqi@gmail.com>
2017-07-23 23:14:40 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+var opt = require('minimist')(process.argv.slice(2));
+
+var mqtt = require('mqtt')
+
+opt.broker = opt.broker || "mqtt://locahost:1883"
+opt.clients = opt.clients || 10 ;
+opt.count = opt.count || 100 ;
+opt.topic = opt.topic || "/mqttjs-bench" ;
+opt.qos = opt.qos || 0 ;
+opt.size = opt.size || 1024 ;
+opt.sleep = opt.sleep || 0
+opt.message = new Array(opt.size+ 1).join('h');
+
+// help functions
+const now = ()=>Date.now()
+const sleep = ()=> new Promise(resolve => setTimeout(resolve, opt.sleep))
+
+
+let clients = []
+let failedClients = 0
+
+const connectToClients = ()=> new Promise((resolve, reject) =>{
+  const c = mqtt.connect(opt.broker,opt)
+  c.on('connect', ()=>resolve(c))
+  c.on('error',  ()=>reject())
+})
+
+const publish = (c, counter = 0 , success = 0) =>{
+  if(opt.sleep > 0){
+    sleep()
+  }
+  if (counter == 0 ) {
+    c.startTime =now()
+  }
+  if (counter >= opt.count ){
+    //bandwidth
+    pushResult(success / (now() - c.startTime) * 1000)
+    return
+  } else {
+    c.publish(opt.topic, opt.message,{qos:opt.qos},(err)=>{
+      !err && success++
+      counter ++;
+      publish(c,counter,success)
+    })
+  }
+}
+
+let results = []
+const pushResult = (result)=>{
+  results.push(result)
+  if(results.length == opt.clients){
+    console.log("bandwidth:",results.reduce((a,b)=>(a+b))/results.length)
+    clients.forEach((c)=>c.end(true))
+  }
+
+}
+
+const start = async ()=> {
+
+  // trying to connect to all clients before starting the test
+  console.log(`trying to conenct to ${opt.clients}...`);
+  while (clients.length < opt.clients && failedClients <opt.clients) {
+    try {
+      clients.push(await connectToClients())
+    } catch (er) {
+      failedClients ++
+    }
+  }
+
+  if (!(failedClients <opt.clients)) {
+    console.log("Please check the broker address could not connect to clients");
+    process.exit(1);
+  }
+  console.log(`Conencted to ${opt.clients} sucessfully`);
+  for (let i = 0 ; i <clients.length ; i++) {
+    publish(clients[i])
+  }
+
+}
+
+start()
