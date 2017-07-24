9a7bee41b1a376cf2bb992fafbf7e36f581398c2
hareeqi <hareeqi@gmail.com>
2017-07-24 02:06:21 +0200

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const opt = require('minimist')(process.argv.slice(2));
 const fs = require('fs');
 const mqtt = require('mqtt')
-opt.broker = opt.broker || "mqtt://locahost:1883"
+opt.broker = opt.broker
+opt.subbroker = opt.subbroker || opt.broker
+opt.submode = opt.submode || false
+opt.subtimeout = opt.subtimeout || 5
 opt.clients = opt.clients || 10 ;
 opt.count = opt.count || 100 ;
 opt.topic = opt.topic || "/mqttjs-bench" ;
@@ -15,13 +18,13 @@ opt.cert = opt.cert &&  fs.readFileSync(opt.cert)
 
 // help functions
 const now = ()=>Date.now()
-const sleep = ()=> new Promise(resolve => setTimeout(resolve, opt.sleep))
+const sleep = (t=opt.sleep)=> new Promise(resolve => setTimeout(resolve, t))
 
 
 let clients = []
 let failedClients = 0
 
-const connectToClients = ()=> new Promise((resolve, reject) =>{
+const connectToClients = (broker=opt.broker)=> new Promise((resolve, reject) =>{
   const c = mqtt.connect(opt.broker,opt)
   c.on('connect', ()=>resolve(c))
   c.on('error',  (err)=>reject(err))
@@ -36,7 +39,7 @@ const publish = async (c, counter = 0 , success = 0) =>{
   }
   if (counter >= opt.count ){
     //bandwidth
-    pushResult(success / (now() - c.startTime) * 1000)
+    opt.submode?subresult(c):pubresult(success / (now() - c.startTime) * 1000)
     return
   } else {
     c.publish(opt.topic, opt.message,{qos:opt.qos},(err)=>{
@@ -47,21 +50,60 @@ const publish = async (c, counter = 0 , success = 0) =>{
     })
   }
 }
+let subs = []
+const markSubbed = async(t)=>{
+  subs.push(t)
+  if(subs.length == opt.clients) {
+    console.log('All clients sucessfully subscribed, startting publish');
+    let c = false
+    while (!c) {
+      try{
+        c = await connectToClients(opt.subbroker)
+      } catch (err) {
+        opt.d && console.log("subscription mode: Couldn't connect to puplish");
+      }
+    }
+    publish(c)
+  }
+}
+
+const subscribe = (c) =>{
+    c.recivedCount = 0
+    c.subscribe(opt.topic,{qos:opt.qos},async(err)=>{
+        if (err) {
+          await sleep(5)
+          subscribe(c)
+        } else {
+          markSubbed(true)
+        }
+    })
+    c.on('message', (topic, message)=>(c.recivedCount++))
+}
 
 let results = []
-const pushResult = (result)=>{
+const pubresult = (result)=>{
   results.push(result)
   if(results.length == opt.clients){
     console.log("Bandwidth (messages/second):",results.reduce((a,b)=>(a+b))/results.length)
     clients.forEach((c)=>c.end(true))
   }
+}
 
+const subresult = async (c)=>{
+  await sleep(opt.subtimeout*1000)
+  const duraion = now()-c.startTime
+
+  const totalAvgMsgRecived = clients.reduce((a,b)=>(a+b.recivedCount),0)/clients.length
+  console.log("total avg recived:",totalAvgMsgRecived);
+  console.log("Bandwidth (messages/second):",totalAvgMsgRecived/duraion*1000)
+  c.end(true)
+  clients.forEach((c)=>c.end(true))
 }
 
 const start = async ()=> {
 
   // trying to connect to all clients before starting the test
-  console.log(`trying to conenct to ${opt.clients}...`);
+  console.log(`trying to conenct to ${opt.clients} clients ...`);
   while (clients.length < opt.clients && failedClients <opt.clients) {
     try {
       clients.push(await connectToClients())
@@ -76,8 +118,9 @@ const start = async ()=> {
     process.exit(1);
   }
   console.log(`Conencted to ${opt.clients} sucessfully`);
+  const action  = opt.submode?subscribe:publish
   for (let i = 0 ; i <clients.length ; i++) {
-    publish(clients[i])
+    action(clients[i])
   }
 
 }
